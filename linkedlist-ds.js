5e66d321aaecdeafcaa070e60de3eaaedc821000
olopok <pabisa.pb@gmail.com>
2025-05-13 20:29:37 +0100

    Added size first and tail methods.

--- linkedlist-ds.js
+++ linkedlist-ds.js
@@ -31,8 +31,42 @@ export default class LinkedList {
 
   prepend(value) {
     const newNode = new Node(value);
-    newNode.next = this.head
-    this.head = newNode
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  get size() {
+    let size = 0;
+    let currentNode = this.head;
+    while (currentNode) {
+      size++;
+      currentNode = currentNode.next;
+    }
+    return size;
+  }
+
+  get first() {
+    // let firstNode;
+    // if (this.head !== null) {
+    //   firstNode = this.head;
+    // }
+    // return  firstNode.value;
+    return this.head.value;
+  }
+
+  tail(value) {
+    const length = value;
+    let last = this.head;
+    if (last.next === null) {
+      return last;
+    } else {
+      let currentNode = last.next;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+        last = currentNode;
+      }
+      return last.value;
+    }
   }
 }
 
--- main.js
+++ main.js
@@ -2,7 +2,20 @@ import LinkedList from "./linkedlist-ds.js";
 
 const newList = new LinkedList();
 
-newList.prepend('gatto');
+newList.append("gatto");
+newList.append(40);
+newList.append(50);
+// console.log(
+//   "This are the values of all the node in the list:",
+//   newList.toString()
+// );
+newList.prepend("gatto");
 newList.prepend(40);
 newList.prepend(50);
 console.log(newList.toString());
+console.log("This is the size of the linked list:", newList.size);
+console.log("This is the value of the Head node on the list:", newList.first);
+console.log(
+  "This is the value of the Tail Node in the list:",
+  newList.tail(newList.size)
+);
